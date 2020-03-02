ecfc86e82c682a6c8c23f3e3875ff2519dd5fe9f
Ruth Obe <ruthobey@gmail.com>
2020-03-02 07:37:39 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,16 +8,19 @@ def load_library(emoticon_file)
   emoticon[:get_emoticon] = {}
   
   srr.each do |key,value|
+    emoticon[:get_meaning][value[1]] = ""
     binding.pry 
   end 
   
   emoticon
 end
 
+#-------------------------------
+
 def get_japanese_emoticon
   # code goes here
 end
-
+#-------------------------------
 def get_english_meaning
   # code goes here
 end
